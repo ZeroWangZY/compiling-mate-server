641b528a0c50072978d8aa61dd45327e0059d189
ZeroWangZY <756762961@qq.com>
2018-06-07 13:06:06 +0800

    use json parameter from frontend instead of urlencoded

--- api/src/main/java/ecnu/compiling/compilingmate/controller/LexController.java
+++ api/src/main/java/ecnu/compiling/compilingmate/controller/LexController.java
@@ -5,10 +5,8 @@ import ecnu.compiling.compilingmate.entity.Result;
 import ecnu.compiling.compilingmate.lex.policy.rule.Rule;
 import ecnu.compiling.compilingmate.service.LexService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+import org.json.*;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -22,26 +20,30 @@ public class LexController {
     @Resource
     LexService lexService;
 
-    @RequestMapping(value = "/scan", method = RequestMethod.POST)
-    @ResponseBody
-    public Result scan(@RequestParam(value = "inputCode") String inputCode,
-                       @RequestParam(value = "reDefs")Set<LanguageDefinition> reDefs){
-        Result result = new Result();
-
-        try {
-            result.setSuccess(true);
-            result.setData(lexService.scanCodeByRule(inputCode, reDefs));
-        } catch (Exception e){
-            result.setSuccess(false);
-            result.setMsg(e.getMessage());
-        }
-
-        return result;
-    }
+//    @RequestMapping(value = "/scan", method = RequestMethod.POST)
+//    @ResponseBody
+//    public Result scan(@RequestParam(value = "inputCode") String inputCode,
+//                       @RequestParam(value = "reDefs")Set<LanguageDefinition> reDefs){
+//        Result result = new Result();
+//
+//        try {
+//            result.setSuccess(true);
+//            result.setData(lexService.scanCodeByRule(inputCode, reDefs));
+//        } catch (Exception e){
+//            result.setSuccess(false);
+//            result.setMsg(e.getMessage());
+//        }
+//
+//        return result;
+//    }
 
     @RequestMapping(value = "/reProcessingOutput", method = RequestMethod.POST)
     @ResponseBody
-    public Result reProcessingOutput(@RequestParam(value = "input") String input, @RequestParam(value = "ruleName", required = false, defaultValue = "") String ruleName){
+    public Result reProcessingOutput(@RequestBody String json){
+        JSONObject jsonObj = new JSONObject(json);
+        String input = jsonObj.getString("input");
+        String ruleName = "";
+
         Result result = new Result();
 
         try {
